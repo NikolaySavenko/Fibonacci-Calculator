3c0b753c91a28ec897a4c09c58eca896a3c6895f
Nikolay Savenko <savenkons@yandex.ru>
2021-05-14 00:20:52 +0300

    schedule update event

---
+++ Fibonacci-Calculator/CalculatorManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Calculator
+{
+	class CalculatorManager
+	{
+		private string[] _schedule;
+		public string[] Schedule {
+			get => _schedule;
+			set {
+				_schedule = value;
+				OnScheduleUpdated(_schedule);
+			} 
+		}
+		public delegate void ScheduleUpdatedDelegate(string[] schedule);
+		public event ScheduleUpdatedDelegate OnScheduleUpdated = (string[] schedule) => { };
+	}
+}
