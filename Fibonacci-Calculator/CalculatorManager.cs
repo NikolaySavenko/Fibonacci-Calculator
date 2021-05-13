ff036a49c3e432226919199c577569e6d87cfa45
Nikolay Savenko <savenkons@yandex.ru>
2021-05-14 00:24:02 +0300

    manager singleton

--- Fibonacci-Calculator/CalculatorManager.cs
+++ Fibonacci-Calculator/CalculatorManager.cs
@@ -8,15 +8,28 @@ namespace Fibonacci_Calculator
 {
 	class CalculatorManager
 	{
-		private string[] _schedule;
-		public string[] Schedule {
+		private CalculatorManager _manager;
+		public CalculatorManager Manager { 
+			get {
+				if (_manager == null) _manager = new CalculatorManager();
+				return _manager;
+			}
+		}
+
+		private List<string> _schedule;
+		public List<string> Schedule {
 			get => _schedule;
 			set {
 				_schedule = value;
 				OnScheduleUpdated(_schedule);
 			} 
 		}
-		public delegate void ScheduleUpdatedDelegate(string[] schedule);
-		public event ScheduleUpdatedDelegate OnScheduleUpdated = (string[] schedule) => { };
+		public delegate void ScheduleUpdatedDelegate(List<string> schedule);
+		public event ScheduleUpdatedDelegate OnScheduleUpdated = (List<string> schedule) => { };
+
+		private CalculatorManager()
+		{
+			_schedule = new List<string>();
+		}
 	}
 }
