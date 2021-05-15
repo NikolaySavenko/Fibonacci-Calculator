9e6dcac42fd1b2b0937c3292cede53abeb5b8732
Nikolay Savenko <savenkons@yandex.ru>
2021-05-16 00:08:19 +0300

    summarize operator

--- FibonacciNumbers/FiboInt/FiboIntOperators.cs
+++ FibonacciNumbers/FiboInt/FiboIntOperators.cs
@@ -6,7 +6,24 @@ namespace FibonacciNumbers.FiboInt
 	public partial struct FiboInt
 	{
 		public static FiboInt operator+(FiboInt a, FiboInt b) {
-			return new FiboInt();
+			if (a.number < b.number) {
+				return b + a;
+			}
+			var num = a;
+			for (var i = num.series.Count; i > 0; i --) {
+				if ((num.number >> i & b.number >> i & 1) > 0)
+				{
+					num.Expand();
+					num.number += Convert.ToUInt64(1 << i);
+					num.Normalize();
+				} else if ((b.number >> i & 1) > 0)
+				{
+					num.number += Convert.ToUInt64(1 << i);
+					num.Normalize();
+					num.series.UpTo((ulong)num);
+				}
+			}
+			return num;
 		}
 
 		public static FiboInt operator -(FiboInt a, FiboInt b)
