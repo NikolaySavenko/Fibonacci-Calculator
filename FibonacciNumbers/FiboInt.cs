d7ae1e2bf91e7dfb5ba45baf49aae0186d05e658
Nikolay Savenko <savenkons@yandex.ru>
2021-05-15 19:05:11 +0300

    ulong in fibonacci binary

--- FibonacciNumbers/FiboInt.cs
+++ FibonacciNumbers/FiboInt.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.Collections;
 
 namespace FibonacciNumbers
 {
 	public struct FiboInt
 	{
 		private readonly UInt64 number;
+		public readonly FibonacciSeries series;
 
-		public FiboInt(int source) {
+		public FiboInt(UInt64 source) {
+			series = new FibonacciSeries();
 			number = 0;
+			series.UpTo(source);
+			for (var i = series.Count - 1; i >= 0; i--) {
+				var fNum = series[i];
+				var bitValue = source / fNum;
+				number += (ulong)(1 << i) * bitValue;
+				source %= fNum;
+			}
 		}
+
+		public override string ToString() => Convert.ToString((long)number, 2);
 	}
 }
