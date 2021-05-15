a275d22d84f92b44396b73307facddb1e2cf4c56
Nikolay Savenko <savenkons@yandex.ru>
2021-05-16 00:08:04 +0300

    Normalize and Expand methods

--- FibonacciNumbers/FiboInt/FiboInt.cs
+++ FibonacciNumbers/FiboInt/FiboInt.cs
@@ -6,7 +6,7 @@ namespace FibonacciNumbers.FiboInt
 {
 	public partial struct FiboInt
 	{
-		private readonly UInt64 number;
+		private UInt64 number;
 		public readonly FibonacciSeries series;
 
 		public FiboInt(UInt64 source) {
@@ -19,6 +19,48 @@ namespace FibonacciNumbers.FiboInt
 				number += (ulong)(1 << i) * bitValue;
 				source %= fNum;
 			}
+		}
+
+		private void Normalize() {
+			for (var i = series.Count - 1; i > 0; i--)
+			{
+				if ((GetBit(i) & GetBit(i - 1)) > 0) {
+					number -= Convert.ToUInt64(3 << (i - 1));
+					number += Convert.ToUInt64(1 << (i + 1));
+				}
+			}
+		}
+
+		private void Expand() {
+			number = (number >> 2) + (number >> 1);
+		}
+
+		public static ulong Expand(ulong number)
+		{
+			return (number >> 2) + (number >> 1);
+		}
+
+		private ulong GetBit(int index)
+		{
+			return (number >> index) & 0x1;
+		}
+
+		public static ulong Normalize(ulong binary)// DANGER !!! ITS A BINARY
+		{
+			var series = new FibonacciSeries(Convert.ToInt32(binary));
+			for (var i = series.Count - 1; i > 0; i--)
+			{
+				if ((GetBit(binary, i) & GetBit(binary, i - 1)) > 0)
+				{
+					binary -= Convert.ToUInt64(3 << (i - 1));
+					binary += Convert.ToUInt64(1 << i + 1);
+				}
+			}
+			return binary;
+		}
+
+		public static ulong GetBit(ulong number, int index) {
+			return (number >> index) & 0x1;
 		}
 
 		public override string ToString() => Convert.ToString((long)number, 2);
