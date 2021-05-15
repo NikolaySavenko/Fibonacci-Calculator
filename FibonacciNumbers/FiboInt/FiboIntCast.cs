76cbaf1f109aa449079b1155c2da41c3361015e5
Nikolay Savenko <savenkons@yandex.ru>
2021-05-15 23:45:25 +0300

    cast fiboInt to uint

---
+++ FibonacciNumbers/FiboInt/FiboIntCast.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+
+namespace FibonacciNumbers.FiboInt
+{
+	public partial struct FiboInt
+	{
+		public static explicit operator uint(FiboInt fInt) {
+			uint result = 0;
+			var series = fInt.series;
+			for (var i = series.Count - 1; i > 0; i--)
+			{
+				result += Convert.ToUInt32(series[i] * (fInt.number >> i & 1));
+			}
+			return result;
+		}
+	}
+}
