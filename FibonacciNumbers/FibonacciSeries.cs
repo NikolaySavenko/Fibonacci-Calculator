80c81680aa4404d74da86202251288ed9b20176d
Nikolay Savenko <savenkons@yandex.ru>
2021-05-15 19:06:33 +0300

    simple UpTo logic

--- FibonacciNumbers/FibonacciSeries.cs
+++ FibonacciNumbers/FibonacciSeries.cs
@@ -8,24 +8,33 @@ namespace FibonacciNumbers
 	{
 		public List<UInt64> Series { get; private set; }
 
+		public int Count { get => Series.Count; }
+
 		public UInt64 N {
 			get => Series[Series.Count - 1];
 		}
 
+		public FibonacciSeries() : this(0) { }
+
 		public FibonacciSeries(int seed) {
 			Series = new List<UInt64>();
-			if (seed > 0)
-			{
-				Series.Add(1);
-				if (seed > 1)
-				{
-					Series.Add(1);
-				}
-				
-				for (var i = 2; i < seed; i++)
-				{
-					Series.Add(Series[i - 1] + Series[i - 2]);
-				}
+			Series.Add(1);
+			Series.Add(1);
+			
+			for (var i = 2; i < seed; i++) {
+				Series.Add(Series[i - 1] + Series[i - 2]);
+
+			}
+		}
+
+		public UInt64 this[int index] {
+			get => Series[index];
+		}
+
+		public void UpTo(UInt64 number)
+		{
+			while (N < number) {
+				Series.Add(N + Series[Series.Count - 2]);
 			}
 		}
 
