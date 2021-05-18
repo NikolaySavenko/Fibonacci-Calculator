9e37da1042cf4cd4e62253557b8200f4274115b8
Nikolay Savenko <savenkons@yandex.ru>
2021-05-18 20:59:10 +0300

    added normal multiply

--- FibonacciNumbers/FiboInt/FiboIntMultiply.cs
+++ FibonacciNumbers/FiboInt/FiboIntMultiply.cs
@@ -10,12 +10,40 @@ namespace FibonacciNumbers.FiboInt
 			if (a.number == 2) return b;
 			if (b.number == 2) return a;
 
-			var series = new FibonacciSeries(a.series.Seed + b.series.Seed);
+			var series = new FibonacciSeries(a.series.Seed + b.series.Seed - 2);
 			var first = new FiboInt(series.N);
-			var nextA = a.series[a.series.Seed - 2];
-			var nextB = a.series[b.series.Seed - 2];
+			var nextA = a.series[a.series.Seed - 3];
+			var nextB = a.series[b.series.Seed - 3];
 			var other = SimpleBinaryMultiply(new FiboInt(nextA), new FiboInt(nextB));
 			return first + other;
+		}
+
+		public static FiboInt operator *(FiboInt a, FiboInt b)
+		{
+			// check for zero or one multiply
+			if (a.number == 2) return b;
+			if (a.number == 0) return new FiboInt(0);
+			if (b.number == 2) return a;
+			if (b.number == 2) return new FiboInt(0);
+
+			// a > 0 and b > 0
+			var num = new FiboInt(0);
+			for (var i = a.series.Count - 1; i >= 0; i--)
+			{
+				if ((a.number >> i & 1) > 0)
+				{
+					for (var j = b.series.Count - 1; j >= 0; j--)
+					{
+						if ((b.number >> j & 1) > 0)
+						{
+							var operand0 = new FiboInt(a.series[i]);
+							var operand1 = new FiboInt(b.series[j]);
+							num += SimpleBinaryMultiply(operand0, operand1);
+						}
+					}
+				}
+			}
+			return num;
 		}
 	}
 }
